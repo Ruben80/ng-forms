e9e55b2c1ba89a985eb6b76c8c9e122c8fb020a0
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-01-11 16:40:19 +0100

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,23 @@
+export const products = [
+	{
+		name: 'Nike Air Max 2000 ng',
+		price: 249,
+		description: 'The best ones ever!'
+	},
+	{
+		name: 'Lasergun invisability shield',
+		price: 279,
+		description: "The best solution for 'lil speed devils"
+	},
+	{
+		name: 'Instant coffee module',
+		price: 69,
+		description: 'Have your perfect tasty coffee with one push of a button '
+	}
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
